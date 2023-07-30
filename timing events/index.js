de6fd45c644fb9ef2c884b3c0f6129a72b951689
Habib16051 <habibmhr143@gmail.com>
2023-07-30 11:43:31 +0600

    git timing events

--- timing events/index.js
+++ timing events/index.js
@@ -7,8 +7,22 @@
 
 // settime out event
 
-setTimeout(display, 2000);
+// setTimeout(display, 2000);
 
-function display(){
-    console.log('hello')
-}
+// function display(){
+//     console.log('hello')
+// }
+
+// startclock
+// () and stop clock functions using setInterval method
+let count = 1; // counter variable to keep track of the number of times interval has been
+// executed
+let intervalId = setInterval(startClock, 1000); // setInterval method to execute the startClock function every 1 second
+function startClock() {
+    console.log("Bangladesh");
+    if (count === 5) {
+        clearInterval(intervalId); // clearInterval method to stop the execution of the setInterval method
+    }
+    count++;
+    }
+
