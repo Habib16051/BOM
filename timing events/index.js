f713998f67c70880d42f73dd4c00da98ed7cf1be
Habib16051 <habibmhr143@gmail.com>
2023-07-30 11:36:13 +0600

    setTimeout

---
+++ timing events/index.js
@@ -0,0 +1,14 @@
+// js timing events
+
+// setTimeout(()=>{
+//     console.log('hello')
+
+// }, 2000)
+
+// settime out event
+
+setTimeout(display, 2000);
+
+function display(){
+    console.log('hello')
+}
