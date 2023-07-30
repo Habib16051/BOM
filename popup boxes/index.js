b109fb0449f75555ab15338e4c7176fc6e66d7b0
Habib16051 <habibmhr143@gmail.com>
2023-07-30 11:16:38 +0600

    pop up boxes

---
+++ popup boxes/index.js
@@ -0,0 +1,22 @@
+// alert 
+// alert("How are you?")
+
+// // confirm
+// confirm("Are your confirm?");
+
+// function deleteSomething(){
+//     let value = confirm("Are you sure to delete it?");
+
+//     if(value){
+//         console.log("Deleted!");
+//     }else{
+//         console.log("Not deleted!");
+//     }
+// }
+// deleteSomething(); 
+
+// Prmopt
+
+let name = prompt("What is your name?");
+console.log(name);
+
